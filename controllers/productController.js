6c12eba7c2b23e518a44598b926ac18275bb112e
waruna925 <warunasuraweera925@gmail.com>
2026-01-24 00:13:36 +0530

    changed ✏️

--- controllers/productController.js
+++ controllers/productController.js
@@ -82,3 +82,72 @@ export async function deleteProduct(req,res) {
         )
     }
 }
+
+export async function updateProduct(req,res){
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message:"You are not authorized to update a product"
+        })
+        return
+    }
+
+    const productId=req.params.productId;
+    const updatingData=req.body;
+
+    try{
+        await Product.updateOne(
+            {productId:productId},
+            updatingData
+        )
+        res.json(
+            {
+                message:"Product updated successfully"
+            }
+        )
+    }
+    catch(err){
+        res.status(500).json({
+            message:"Failed to update product",
+            error:err
+        })
+    }
+}
+
+async function getProductById(req,res) {
+    const productId=req.params.productId
+
+    try{
+        const product =await Product.findOne(
+            {productid:productId}
+        )
+
+        if(product==null){
+            res.status(404).json(
+                {
+                    message:"Product not found"
+                }
+            )
+            return
+        }
+        if(product.isAvailable){
+            res.json(product)
+        }else{
+            if(!isAdmin(req)){
+                res.status(404).json(
+                    {
+                        message:"Product not found"
+                    }
+                )
+                return
+            }else{
+                res.json(product)
+            }
+        }
+    }
+    catch(e){
+        res.status(500).json({
+            message:"Failed to update product",
+            error:err
+        })
+    }
+}
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {getProduct, saveProduct ,deleteProduct } from '../controllers/productController.js';
+import {getProduct, saveProduct ,deleteProduct , updateProduct} from '../controllers/productController.js';
 
 const productRouter=express.Router();
 
 productRouter.get("/",getProduct)
 productRouter.post("/",saveProduct)
 productRouter.delete("/:productId",deleteProduct)
+productRouter.put("/:productId",updateProduct)
 
 export default productRouter
