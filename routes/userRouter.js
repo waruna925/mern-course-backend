369b49b1b78fdb98c1899b4888f8c95548ff86d6
waruna925 <warunasuraweera925@gmail.com>
2026-07-11 01:42:39 +0530

    Add google login and Forgot Password ✏️

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createUser, loginUser } from '../controllers/userController.js';
+import { createUser, loginUser, loginWithGoogle, resetPassword, sendOTP } from '../controllers/userController.js';
 
 const userRouter=express.Router();
 
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
+userRouter.post("/login/google",loginWithGoogle)
+userRouter.post("/send-otp",sendOTP)
+userRouter.post("/reset-password",resetPassword)
 
 export default userRouter;
